0eb18f6457cfd5a2a30bf0e9a48cccc4cc22f73e
ALE <alejandrodmt@uci.cu>
2023-06-26 22:29:29 -0500

    gateway2

--- usuario-service/src/main/java/com/mio/usuarioservice/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/mio/usuarioservice/feignclients/CarroFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@FeignClient(name = "carro-service",url = "http://localhost:8002")
+@FeignClient(name = "carro-service")
 @RequestMapping("/carro")
 public interface CarroFeignClient {
 
